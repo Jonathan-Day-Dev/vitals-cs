58b7a4918233b3eab545847535ab2a130c7b2c77
Jonathan Day <jonday@jonday.dev>
2021-11-07 07:09:44 -0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from './components/Home.vue';
+
+const app = createApp(App)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+             path: "/",
+             component: Home
+        }
+    ]
+})
+
+app.use(router);
+
+app.mount('#app')
